60abff6e7ad781f751987af98f4970fd0813b022
unknown <stoixxxman>
2019-05-26 13:18:25 +0400

    Копипаст

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-console.log('hello world');
-
-console.log('my name srt-bot');
-
-console.log('Вроде норм работает 1');
+// Make sure we got a filename on the command line.
+if (process.argv.length < 3) {
+    console.log('Usage: node ' + process.argv[1] + ' FILENAME');
+    process.exit(1);
+  }
+  // Read the file and print its contents.
+  var fs = require('fs')
+    , filename = process.argv[2];
+  fs.readFile(filename, 'utf8', function(err, data) {
+    if (err) throw err;
+    console.log('OK: ' + filename);
+    console.log(data)
+  });
 
