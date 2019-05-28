446c39eeb28ba25b9b10ef609e5ec1aa008cf73b
Vikentii_Ekgart <Vikentii_Ekgart@epam.com>
2019-05-28 20:46:35 +0400

    splitted by words

--- index.js
+++ index.js
@@ -7,13 +7,29 @@ if (process.argv.length < 3) {
 // Read the file and print its contents.
 var fs = require('fs'), filename = process.argv[2];
 
+
 fs.readFile(filename, 'utf8', function (err, data) {
   if (err) throw err;
-  const parseSubs =  parse(data);
-  console.log('OK: ' + filename);
-  console.log(data);
-  console.log(parse(data));
+  const parsedSubs = parse(data);
+
+
+  const phrase = parsedSubs[0].text;
+
+  const arrayOfWords = phrase.split(/[' '|'\n']/i);
+  const cleanedArray = [];
+  arrayOfWords.forEach((el) => {
+    cleanedArray.push(el.replace(/[,|\.|\!]/i, ''));
+  });
+
+  console.log(arrayOfWords);
+  console.log(cleanedArray);
+
+  //console.log(parsedSubs[0].text);
+
+  //console.log(parsedSubs);
 });
+
+
 
 
 
