8833840e70f69a3245a58e05bf4fa922ee2861e3
stoixxxman <stoixxxman@gmail.com>
2019-12-31 01:59:37 +0400

    googleTTS.js edit

--- src/googleTTS.js
+++ src/googleTTS.js
@@ -1,17 +1,15 @@
+var fs = require('fs');
+
 const json = require('./srt-bot.json');
 // Imports the Google Cloud client library
 const textToSpeech = require('@google-cloud/text-to-speech');
-
 // Import other required libraries
-const fs = require('fs');
 const util = require('util');
 async function main(phrase) {
   // Creates a client
   const client = new textToSpeech.TextToSpeechClient();
-
   // The text to synthesize
   const text = phrase;
-
   // Construct the request
   const request = {
     input: {text: text},
@@ -20,12 +18,21 @@ async function main(phrase) {
     // Select the type of audio encoding
     audioConfig: {audioEncoding: 'MP3'},
   };
-
   // Performs the Text-to-Speech request
   const [response] = await client.synthesizeSpeech(request);
   // Write the binary audio content to a local file
   const writeFile = util.promisify(fs.writeFile);
-  await writeFile(`${text}.mp3`, response.audioContent, 'binary');
+  await writeFile(`sourse/mp3/${text}.mp3`, response.audioContent, 'binary');
   console.log(`Audio content written to file: ${text}.mp3`);
 }
-main();
+
+async function mainStarter(phrase){
+  try{
+    let oneSub = await main(phrase);
+    console.log(oneSub);
+  } catch(err){
+    console.log(err);
+  }
+} 
+
+module.exports = mainStarter();
