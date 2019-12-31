3a9cfc2153c760b134b57600967a5ef4abdc4bc6
stoixxxman <stoixxxman@gmail.com>
2019-12-31 21:39:35 +0400

    tts work now

--- make output mp3.bat
+++ make output mp3.bat
@@ -1,6 +1,9 @@
 ffmpeg -f lavfi -i anullsrc=r=44100:cl=mono -t 3 -q:a 9 -acodec libmp3lame silence3sec.mp3
-ffmpeg -f concat -safe 0 -i mylistMP3.txt -c copy 1output.mp3
-ffmpeg -f concat -safe 0 -i mylistMP3.txt -acodec copy 1output.wav
+ffmpeg -f lavfi -i aevalsrc=0:duration=2 -ab 320k silence.mp3
+ffmpeg -i input.wav -af "apad=pad_dur=3" output.m4a
+ffmpeg -auto_convert 1 -f concat -i glueAudioFFMPEG.bat -c copy 1output.m4a
+
+ffmpeg -f concat -safe 0 -i glueAudioFFMPEG.txt -acodec copy 1output.wav
 ffmpeg -loop 1 -y -i blue.png -i 1output.wav -shortest -acodec copy -vcodec mjpeg 2result.avi
 ffmpeg -i 2result.avi 3result.mp4 
 pause
--- src/readFile.js
+++ src/readFile.js
@@ -47,7 +47,9 @@ if (process.argv.length < 3) {
   let phrase = [];
   let forOutCleanArray = [];
   let phraseArray = [];
-  fs.appendFileSync('./sourse/mp3/pronunciation-finder.bat', `pronunciation-finder `); 
+  fs.appendFileSync('./sourse/mp3/pronunciation-finder.bat', `pronunciation-finder `);
+  fs.appendFileSync('./sourse/mp3/trackWithSilence.bat', `ffmpeg \-f lavfi \-i aevalsrc=0:duration=2 \-ab 320k silence.mp3 \n`);
+ 
   fs.readFile(filename, 'utf8', function (err, data) {
     if (err) throw err;
     parsedSubs = parse(data);
@@ -55,7 +57,8 @@ if (process.argv.length < 3) {
     var arrEl = [];
     for (let i = 0; i < parsedSubs.length; i += 1) {
         phrase = parsedSubs[i].text;
-        phrase = phrase.replace(/<i>/gi, '').replace(/<\/i>/gi, '').replace(/\\n\'/gi, '').replace(/[^\w .]/gi, '');
+        phrase = phrase.replace(/<i>/gi, '').replace(/<\/i>/gi, '').replace(/\\n\'/gi, ' ').replace(/[^\w ]/gi, '');
+        let contrlPhrase = phrase.replace(/ /gi,'_');
         mainStarter(phrase);
         phraseArray[i] = phrase;
         const arrayOfWords = phrase.split(/[' '|'\n'|.|!|?|,]/i); 
@@ -66,15 +69,16 @@ if (process.argv.length < 3) {
             arrEl.push(el);
             mainStarter(el);
             fs.appendFileSync('./sourse/sortCleanedArray.txt', `${el} \n`);
-            fs.appendFileSync('./sourse/mp3/glueAudioFFMPEG.bat', `file '${el}.mp3' \n`);
-            fs.appendFileSync('./sourse/mp3/glueAudioFFMPEG.bat', `file \'silence3sec.mp3\' \n`);
+            fs.appendFileSync('./sourse/mp3/trackWithSilence.bat', `ffmpeg -i "${el}.mp3" -af "apad=pad_dur=2" ${el}_S.m4a \n`);
+            fs.appendFileSync('./sourse/mp3/glueAudioFFMPEG.bat', `file "${el}_S.m4a" \n`);
+            //fs.appendFileSync('./sourse/mp3/glueAudioFFMPEG.bat', `file \'silence3sec.mp3\' \n`);
             fs.appendFileSync('./sourse/mp3/pronunciation-finder.bat', ` ${el} `);
             return el;
         };
         });
         fs.appendFileSync('./sourse/sortCleanedArray.txt', `${phrase} \n`);
-        fs.appendFileSync('./sourse/mp3/glueAudioFFMPEG.bat', `file \'${phrase}.mp3\' \n`);
-        fs.appendFileSync('./sourse/mp3/glueAudioFFMPEG.bat', `file \'silence3sec.mp3\' \n`);
+        fs.appendFileSync('./sourse/mp3/trackWithSilence.bat', `ffmpeg -i "${phrase}.mp3" -af "apad=pad_dur=4" "${contrlPhrase}_S.m4a" \n`);
+        fs.appendFileSync('./sourse/mp3/glueAudioFFMPEG.bat', `file "${contrlPhrase}_S.m4a" \n`);
         fs.appendFileSync('./sourse/sortCleanedArray.txt', `\n`);
         console.log(phrase);
         wordsFromSub = unique(wordsFromSub);
