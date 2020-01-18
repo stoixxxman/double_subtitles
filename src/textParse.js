aa74e4abd6519d9627c7f3893a579b96462c82ab
stoixxxman <stoixxxman@gmail.com>
2020-01-18 14:26:15 +0400

    work with npm compromise module

--- src/textParse.js
+++ src/textParse.js
@@ -4,6 +4,7 @@ var fs = require('fs');
 const tts = require('./googleTTS.js');
 const nlp = require('compromise');
 nlp.extend(require('compromise-adjectives'));
+nlp.extend(require('compromise-ngrams'));
 
 function fromSrt(parsedSubs, parseUserWords) {
     var arrWords = [];
@@ -11,77 +12,56 @@ function fromSrt(parsedSubs, parseUserWords) {
     let wholeText = '';
     let editText = [];
     let allWords = '';
-    let helfPhraseOne = '';
-    let helfPhraseTwo = '';
-    let arrRemovedWords = [];
+
     for (let i = 0; i < parsedSubs.length; i += 1) {
         subText = parsedSubs[i].text;
         subText = subText.replace(/<i>/gi, '').replace(/<\/i>/gi, '').replace(/\r?\n/g, ' ').replace(/ {1,}/g, ' ').replace(/^\s*/, '').replace(/\s*$/, '');
         wholeText = wholeText + subText + ' ';
     }
     wholeText = wholeText.replace(/[^a-zA-Z ?,.']/g, '');
-    let arrSentence = [];
-    let arrPhrase = wholeText.split(/[.,?!]/);
+    const nlpWholeText = nlp(wholeText);
+    const ngramsForWholeText = nlp(wholeText);
+    
+    console.log(nlp(wholeText).ngrams({ size: 4 }));
 
-    arrPhrase = arrPhrase.map((phrase) => {
-        phrase = phrase.replace(/^\s*/, '').replace(/\s*$/, '');
-        arrWords = phrase.split(' ');
-        for (const i in arrWords) {
-            arrWords[i] = arrWords[i].replace().replace(/\'(.*)\w/,'').replace(/^\s*/, '').replace(/\s*$/, '').toLowerCase();
-            let iWord = nlp(arrWords[i]);
-            iWord.verbs().toInfinitive();
-            iWord.nouns().toSingular();
-            arrWords[i]= iWord.text();    
+    nlpWholeText.verbs().toInfinitive();
+    nlpWholeText.nouns().toSingular();
+    let termSortByLengthAndFreq = nlpWholeText.terms().sort((a, b) => {
+        //sort by length of normalized text
+        if (a.text('reduced').length > b.text('reduced').length) {
+            return -1
+        }
+        return 1
+    }).out('freq').map((d) => {
+        if (d.reduced.length > 3) {
+            return d;
+        }
+
+    }).filter(function (item) {
+        return item != undefined && !parseUserWords.includes(item.reduced); 
             
-            if (phrase.split(' ').length > 4) {
-                let middle = Math.floor(phrase.length / 2);
-                let before = phrase.lastIndexOf(' ', middle);
-                let after = phrase.indexOf(' ', middle + 1);
-
-                if (middle - before < after - middle) {
-                    middle = before;
-                } else {
-                    middle = after;
-                }
-
-                helfPhraseOne = phrase.substr(0, middle);
-                helfPhraseTwo = phrase.substr(middle + 1);
-            }
-            if (!parseUserWords.includes(arrWords[i]) && !editText.includes(phrase)) {
-                if (arrWords.length <= 4) {
-                    //console.log(arrWords[i], '\n', phrase);
-                    editText.push(phrase);
-                    
-                }
-                else if (!parseUserWords.includes(arrWords[i]) && !editText.includes(helfPhraseOne) && !editText.includes(helfPhraseTwo)) {
-                    //console.log(arrWords[i], '\n', 'Part 1 : ', helfPhraseOne, 'Part 2 : ', helfPhraseTwo);
-                    editText.push(helfPhraseOne);
-                    editText.push(helfPhraseTwo);
-                    
-                }
-            }
-        }
-
-
     });
+    
 
-    console.log(wholeText,'\n',editText);
-    fs.appendFileSync('./sourse/wholeText.txt', `${editText}`);
-    let frequ = arrFrequency.getFrequency2(allWords, 20);
-    fs.appendFileSync('./sourse/mp3/output/frequWords.txt', frequ);
-    let topWords = [];
-    topWords = arrWords;
-    topWords.sort(function (a, b) {
-        return b.length - a.length;
-    });
+    fs.appendFileSync('./sourse/File 1 wholeText.txt', `$`);
+    fs.appendFileSync('./sourse/File 2 subsInCourse.txt', `$`);
+    fs.appendFileSync('./sourse/File 3 ngrams.txt', `$`);
 
-    let times = 2;
-    topWords.map((word) => {
-        for (let i = 1; i <= times; i = i + 1) {
-            fs.appendFileSync('./sourse/mp3/topWords.txt', `file ${word}.m4a \n`);
-            fs.appendFileSync('./sourse/mp3/output/topWords.txt', `${word} \n`);
-        }
-    });
+    fs.appendFileSync('./sourse/ngramsForWholeText.txt', `${ngramsForWholeText}`);
+    fs.appendFileSync('./sourse/termSortByLength.txt', `${termSortByLengthAndFreq.map(d => d.reduced)}`);
+
+
 }
 
 module.exports.fromSrt = fromSrt;
+const defaults = {
+    trim: false,          // remove leading/trailing whitespace
+    whitespace: false,    // tabs, double-spaces
+    unicode: false,       // ü → u
+    lowercase: false,     // co-erce everything to lowercase
+    titlecase: false,     // titlecase proper-nouns, acronyms, sentence-starts
+    punctuation: false,   // '?!' → ?
+    acronyms: false,      // F.B.I. → FBI
+    abbreviations: false, // Mrs. → Mrs
+    implicit: false,      // didn't → 'did not'
+}
